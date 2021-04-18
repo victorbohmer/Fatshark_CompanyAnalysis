3e518238d8f172d4bec72f75e2def68cbb12a234
victorbohmer <victorbohmer@gmail.com>
2021-04-18 13:57:37 +0200

    Added reading of csv data from file and parsing to companies

---
+++ Fatshark_CompanyAnalysis/Data/DataHandler.cs
@@ -0,0 +1,25 @@
+﻿using Fatshark_CompanyAnalysis.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fatshark_CompanyAnalysis.Data
+{
+    public class DataHandler
+    {
+        public void ReadCompaniesFromFile(string filePath = null)
+        {
+            if (filePath == null)
+                filePath = Path.Combine(Environment.CurrentDirectory, @"Files\", "uk-500.csv");
+
+            var csvLines = File.ReadAllLines(filePath).Skip(1);
+
+            var companies = csvLines.Select(x => Company.FromCsv(x)).ToArray();
+
+            Console.WriteLine();
+        }
+    }
+}
--- Fatshark_CompanyAnalysis/MainWindow.xaml.cs
+++ Fatshark_CompanyAnalysis/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Fatshark_CompanyAnalysis.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,12 @@ namespace Fatshark_CompanyAnalysis
     /// </summary>
     public partial class MainWindow : Window
     {
+        public DataHandler DataHandler;
         public MainWindow()
         {
             InitializeComponent();
+            DataHandler = new DataHandler();
+            DataHandler.ReadCompaniesFromFile();
         }
     }
 }
